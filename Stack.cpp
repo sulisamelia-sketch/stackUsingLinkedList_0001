54a2418c16f00d2fb3b25c20d39eb157c4ec4a57
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-05-14 14:53:25 +0700

    Menambahkan proses insert node pada push

--- Stack.cpp
+++ Stack.cpp
@@ -27,4 +27,8 @@ public:
 
      int push(int value)
     {
-       
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+      
