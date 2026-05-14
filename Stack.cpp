f8eed628fdf035c2a5008cce4cb170277973a958
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-05-14 14:58:59 +0700

    Menambahkan validasi dan traversal pada peek

--- Stack.cpp
+++ Stack.cpp
@@ -50,4 +50,12 @@ public:
 
     void peek()
     {
-       
+        if (top == NULL)
+        {
+            cout << "List is empty." << endl;
+        }
+         else
+        {
+            Node *current = top;
+            while (current != NULL)
+           
